32ce3df3146d9fda64ecbe68d31b5eaa0487441b
Dutchie1990 <danielboone1990@gmail.com>
2021-01-14 17:53:22 +0000

    rendering information from github

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,20 @@
+function userInformationHtml(user){
+    return `<h2>${user.name}
+                <span class="small-name">
+                    @ <a href="${user.html_url}" target="_blank">${user.login}</a>
+                </span>
+            </h2>
+            <div class="gh-content">
+                <div class="gh-avatar">
+                    <a href=${user.html_url} target="_blank">
+                        <img src=${user.avatar_url} width="80" height="80" alt="${user.login}"/>
+                    </a>
+                </div>
+                <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+            </div>`
+
+}
+
 function fetchGitHubInformation(event){
 
     var userName = $("#gh-username").val();
@@ -9,5 +26,17 @@ function fetchGitHubInformation(event){
 
     $("#gh-user-data").html(`<div id="loader"><img src="assets/images/g0R9.gif" alt="loading"></div>`)
 
+    $.when($.getJSON(`https://api.github.com/users/${userName}`))
+    .then(function(response){
+        var userDate = response;
+        $("#gh-user-data").html(userInformationHtml(userDate));},
+        function(errorResponse){
+            if (errorResponse.status === 404){
+                $("#gh-user-data").html(`<h2>User not found for user: ${userName}</h2>`)
+            } else{
+                console.log(errorResponse);
+                $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
+            }
+    })
 }
 
